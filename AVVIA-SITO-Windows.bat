95d1fe6566ae9b225cd68557b59338ef16168e20
Bizpower SRLS <info@bizpower.it>
2026-07-13 01:05:34 +0200

    Delete AVVIA-SITO-Windows.bat

--- AVVIA-SITO-Windows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo === UP Immobiliare - avvio sito in locale ===
-where node >nul 2>nul || (echo Installa prima Node.js da https://nodejs.org e riavvia questo file. & pause & exit /b)
-call npm install
-call npm run dev
-pause
